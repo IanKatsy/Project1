eb95b1ef0a970c73cc669250f9c089e93e18e275
ian <yiannis7k@gmail.com>
2023-05-12 12:19:19 +0300

    111

--- src/printGrid.c
+++ src/printGrid.c
@@ -76,6 +76,57 @@ void printGrid(CELL **grid) {
     }
 
 }
-void explosion(CELL **grid){
+void explosion(CELL **grid) {
+    int i, j;
 
+    for (i = 0; i < dimV; i++) {
+        for (j = 0; j < dimH; j++) {
+            if (grid[i][j].isMarked && grid[i][j].val == '@') {
+                printf("%*s", WIDTH_DEFAULT, EXPL_BOMB_FOUND);
+            } else if (grid[i][j].isMarked && grid[i][j].val != '@') {
+                printf("%*s", WIDTH_DEFAULT, EXPL_ISNT_BOMB);
+            } else if (grid[i][j].isMarked == 0 && grid[i][j].val == '@') {
+                printf("%*s", WIDTH_DEFAULT, EXPL_BOMB_HIDDEN);
+            } else {
+                switch (grid[i][j].val) {
+                    case CHAR_EMPTY:
+                        printf("%*c", WIDTH_DEFAULT, CHAR_EMPTY);
+                        break;
+
+                    case '1':
+                        printf("%*s", WIDTH_DEFAULT, MS_ONE);
+                        break;
+
+                    case '2':
+                        printf("%*s", WIDTH_DEFAULT, MS_TWO);
+                        break;
+
+                    case '3':
+                        printf("%*s", WIDTH_DEFAULT, MS_THREE);
+                        break;
+
+                    case '4':
+                        printf("%*s", WIDTH_DEFAULT, MS_FOUR);
+                        break;
+
+                    case '5':
+                        printf("%*s", WIDTH_DEFAULT, MS_FIVE);
+                        break;
+
+                    case '6':
+                        printf("%*s", WIDTH_DEFAULT, MS_SIX);
+                        break;
+
+                    case '7':
+                        printf("%*s", WIDTH_DEFAULT, MS_SEVEN);
+                        break;
+
+                    case '8':
+                        printf("%*s", WIDTH_DEFAULT, MS_EIGHT);
+                        break;
+
+                }
+            }
+        }
+    }
 }
